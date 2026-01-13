11c0cfcafd8d611c179d02db686e37a0b4aeea4c
VISHAL SINGH <156414393+ethicalvishal@users.noreply.github.com>
2026-01-14 03:14:53 +0530

    Production settings

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$-)touwa3$+($&&@5o6q2)8(z-l&rg-_x(u(c^!+0q4d1zv$lx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
