8ab02112894ca83261cf1d77ed6a58333f85ce73
VISHAL SINGH <156414393+ethicalvishal@users.noreply.github.com>
2026-01-14 11:52:24 +0530

    date format change

--- expenses/forms.py
+++ expenses/forms.py
@@ -2,9 +2,12 @@ from django import forms
 from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
+    date = forms.DateField(
+        widget = forms.DateInput(
+            attrs = {'type': 'date', 'class': 'form-control'}
+        )
+    )
+    
     class Meta:
         model = Expense
         fields = ['amount', 'category', 'description', 'date']
-        widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'})
-        }
